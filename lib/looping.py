33dedeef54743d48852ac652e33196791465b03e
Abdullahi <aadahiye@gmail.com>
2024-12-19 18:34:59 +0300

    Completes lab on control flow

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count >0:
+        print(count)
+        count-= 1
+    print("Happy New Year!") 
 
 def square_integers(int_list):
     # code goes here!
-    pass
-
+    squared_list = []
+    for i in int_list:
+        squared_list.append(i*i)
+    return squared_list
 def fizzbuzz():
     # code goes here!
-    pass
+    count = 1
+    while count <= 100:
+        if count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print("Buzz")
+        else:
+            print(count)
+        count+= 1
+
